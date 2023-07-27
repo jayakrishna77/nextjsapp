020f82e67e85d0da2fc9184de8b6d7d74121d509
Jayakrishna <Katam.Jayakrishna1@mdlz.com>
2023-07-27 18:29:29 +0530

    secound commit

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,8 +1,35 @@
+import Button from "@/components/button/Button";
+import Image from "next/image";
 import React from "react";
+import styles from "./about.module.css"
 
 const About = () => {
  return(
-    <div>About</div>
+    <div className={styles?.container}>
+<div className={styles?.imgContainer}>
+<Image
+src=""
+fill={true}
+alt=""
+className={styles?.img}
+/>
+<div className={styles?.imgText}>
+<h1 className={styles?.imgTitle}>Digital Storytellers</h1>
+<h2 className={styles?.imgDesc}>Handcrafting award winning digital</h2>
+</div>
+</div>
+<div className={styles?.textContainer}>
+    <div className={styles?.item}>
+        <h1 className={styles?.title}>Who Are We?</h1>
+        <p className={styles?.desc}></p>
+    </div>
+    <div className={styles?.item}>
+    <h1 className={styles?.title}>What We Do?</h1>
+        <p className={styles?.desc}></p>
+        <Button url={"/contact"} text={"Contact"} />
+    </div>
+</div>
+    </div>
  )
 }
 
--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,9 +1,74 @@
 import React from "react";
+import styles from "./category.module.css"
+import Button from "@/components/button/Button";
+import Image from "next/image";
 
-const Category = () => {
-    return (
-        <div>Category</div>
-    )
+const Category = ({ params }) => {
+   return (
+      <div className={styles?.container}>
+         <h1 className={styles?.catTitle}>{params.category}</h1>
+         <div className={styles?.item}>
+            <div className={styles?.content}>
+               <h1 className={styles?.title}>Test</h1>
+               <p className={styles?.desc}>Desc</p>
+               <Button text={"See More"} url={"#"} />
+            </div>
+            <div className={styles?.imgContainer}>
+               <Image
+                  className={styles?.img}
+                  fill={true}
+                  src={""}
+                  alt=""
+               />
+            </div>
+         </div>
+         <div className={styles?.item}>
+            <div className={styles?.content}>
+               <h1 className={styles?.title}>Test</h1>
+               <p className={styles?.desc}>Desc</p>
+               <Button text={"See More"} url={"#"} />
+            </div>
+            <div className={styles?.imgContainer}>
+               <Image
+                  className={styles?.img}
+                  fill={true}
+                  src={""}
+                  alt=""
+               />
+            </div>
+         </div>
+         <div className={styles?.item}>
+            <div className={styles?.content}>
+               <h1 className={styles?.title}>Test</h1>
+               <p className={styles?.desc}>Desc</p>
+               <Button text={"See More"} url={"#"} />
+            </div>
+            <div className={styles?.imgContainer}>
+               <Image
+                  className={styles?.img}
+                  fill={true}
+                  src={""}
+                  alt=""
+               />
+            </div>
+         </div>
+         <div className={styles?.item}>
+            <div className={styles?.content}>
+               <h1 className={styles?.title}>Test</h1>
+               <p className={styles?.desc}>Desc</p>
+               <Button text={"See More"} url={"#"} />
+            </div>
+            <div className={styles?.imgContainer}>
+               <Image
+                  className={styles?.img}
+                  fill={true}
+                  src={""}
+                  alt=""
+               />
+            </div>
+         </div>
+      </div>
+   )
 }
 
 export default Category;
