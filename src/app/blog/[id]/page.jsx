a2d3fd8785219aaee62f7b35747db5b45b7197ac
rijvan <rijvan.mohammad@indovations.in>
2023-07-31 08:34:45 +0530

    images name changed

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,8 +1,42 @@
+import Image from "next/image";
 import React from "react";
+import styles from "./page.module.css"
 
 const BlogPost = () => {
     return (
-        <div>BlogPost</div>
+        <div className={styles?.container}>
+            <div className={styles?.top}>
+                <div className={styles?.info}>
+                    <h1 className={styles?.title}>
+
+                    </h1>
+                    <p className={styles?.desc}>
+
+                    </p>
+                    <div className={styles?.author}>
+                      <Image 
+                      src={"/8.jpeg"} 
+                      alt="" 
+                      width={40} 
+                      height={40} 
+                      className={styles?.avatar}
+                      />
+                      <span className={styles?.username}>Jhon Doe</span>
+                    </div>
+                </div>
+                <div className={styles?.imageContainer}>
+                    <Image
+                      src={"/7.jpeg"}
+                      alt=""
+                      fill={true}
+                      className={styles?.image}
+                    />
+                </div>
+            </div>
+            <div className={styles?.content}>
+                <p className={styles?.text}></p>
+            </div>
+        </div>
     )
 }
 
